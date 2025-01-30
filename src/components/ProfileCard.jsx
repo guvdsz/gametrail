98a82e8f2a8043d8e58aa77ffebdb60059e74d9b
Gustavo Viana <167935951+guvdsz@users.noreply.github.com>
2025-01-29 22:44:02 -0300

    UI Updates

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,10 +1,10 @@
 export default function ProfileCard({ icon, title, value }) {
   return (
-    <div className="flex bg-[#12141C] w-9/10 h-32.5 rounded-lg py-7.5 px-9 items-center max-w-150 gap-5 border-1 border-purple-500/25 hover:border-purple-500/50 transition-colors">
+    <div className="flex bg-[#12141C] w-9/10 h-30 rounded-lg py-7.5 px-9 items-center max-w-150 gap-6 border-1 border-purple-500/25 hover:border-purple-500/50 transition-colors">
       <div className="p-3 rounded-xl bg-purple-700">{icon}</div>
-      <div className="flex flex-col gap-1">
-        <h2 className="text-gray-400 text-xl">{title}</h2>
-        <span className="font-black text-white text-2xl">{value}</span>
+      <div className="flex flex-col">
+        <h2 className="text-gray-400 text-lg">{title}</h2>
+        <span className="font-black text-white text-xl">{value}</span>
       </div>
     </div>
   );
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,19 +2,19 @@ import { User, Gamepad, Trophy, Star, Timer, Clock } from "lucide-react";
 import ProfileCard from "../components/ProfileCard";
 export default function Profile() {
   return (
-    <div className="w-full flex flex-col items-center gap-5">
+    <div className="w-full flex flex-col items-center gap-4">
       <div className="flex flex-col items-center gap-3">
-        <div className="flex flex-col justify-center w-25 h-25 rounded-full overflow-hidden bg-gradient-to-br from-purple-500 to-purple-700 p-1">
+        <div className="flex flex-col justify-center w-20 h-20 rounded-full overflow-hidden bg-purple-700 p-1">
           <div className="w-full h-full bg-[#12141C] rounded-full flex items-center justify-center">
             <User className="w-10 h-10 text-purple-500" />
           </div>
         </div>
         <div className="mb-4 flex flex-col items-center">
-          <h2 className="text-3xl font-bold text-white">User</h2>
-          <p className="text-purple-200">Joined 2025</p>
+          <h2 className="text-2xl font-bold text-white">User</h2>
+          <p className="text-purple-200 text-sm">Joined 2025</p>
         </div>
       </div>
-      <div className="w-full flex flex-col items-center gap-6">
+      <div className="w-full flex flex-col items-center gap-5">
         <ProfileCard title="Total Games" icon={<Gamepad className="text-white" />} value={42}/>
         <ProfileCard title="Completed Games" icon={<Trophy className="text-white" />} value={42}/>
       </div>
